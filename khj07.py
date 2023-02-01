e4d2ab37b3b4d50267bcf6e3255a3b652bfd54ed
mochacof <123149594+mochacof@users.noreply.github.com>
2023-02-01 17:17:47 +0900

    Update khj07.py

--- khj07.py
+++ khj07.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+st.sidebar.success("Select a demo above.")
 def  plotting_demo():
     
     #uploaded_file = st.file_uploader("Choose a file")
