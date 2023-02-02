55d623daacc6c540b84f3b154ee0d009955b124b
mochacof <123149594+mochacof@users.noreply.github.com>
2023-02-02 16:29:25 +0900

    Update khj07.py

--- khj07.py
+++ khj07.py
@@ -20,7 +20,8 @@ def  plotting_demo():
     st.write('You selected:', option)
 
     money = money[:] [money['A_YEAR']== option2]
-
+    global aa
+    aa = money
     fig, ax = plt.subplots(2,2, figsize=(12,8))
 
     plt.subplot(221)
@@ -45,7 +46,7 @@ def  plotting_demo():
     plt.title('House Price')
 
     st.pyplot(fig)
-    st.dataframe(money)
+    #st.dataframe(money)
        
 
 def bar_chart():
@@ -98,11 +99,14 @@ with st.form(key ='Form1'):
         
         
 if select_language =='금리와 집값 빠르게 파악하기':           
-    try:
-          plotting_demo()  
-    except:      
-          pass
-
+   tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+   
+    with tab1:
+        tab1.subheader("A tab with a chart")
+        plotting_demo()
         
+    with tab2:
+        tab2.subheader("A tab with the data")
+        st.dataframe(aa)
 elif select_language =='야구 순위와 승률 빠르게 파악하기':
     bar_chart()
